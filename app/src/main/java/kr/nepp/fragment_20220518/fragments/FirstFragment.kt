47f03a77f9c502a45b8279bcb01bf17750bdaad5
임백산 <turuc@naver.com>
2022-05-18 11:19:59 +0900

    프래그머트의 동작 코드 작성.

--- app/src/main/java/kr/nepp/fragment_20220518/fragments/FirstFragment.kt
+++ app/src/main/java/kr/nepp/fragment_20220518/fragments/FirstFragment.kt
@@ -1,10 +1,12 @@
 package kr.nepp.fragment_20220518.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_first.*
 import kr.nepp.fragment_20220518.R
 
 class FirstFragment : Fragment() {
@@ -17,5 +19,16 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate( R.layout.fragment_first, container, false)
+    }
+
+//    동작 관련 코드 전담 함수
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        btnLog.setOnClickListener {
+
+            Log.d("첫 프래그먼트", "프래그먼트에서의 로그")
+        }
     }
 }
